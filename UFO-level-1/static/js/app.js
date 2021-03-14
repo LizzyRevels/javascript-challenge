46bfe854bb059fdd6031025842f4141b8a4266d7
Lizzy Revels <lizzyrevels1@gmail.com>
2021-03-13 23:58:15 -0500

    pushing index with table and filter js but filter not filtering

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -7,11 +7,11 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 // Console.log the sighting data from data.js
-console.log(data);
+console.log(tableData);
 
 // Step 1: Loop Through `data` and console.log each sighting report object
-data.forEach(function(data) {
-  console.log(data);
+data.forEach(function(tableData) {
+  console.log(tableData);
 });
 
 // Step 2:  Use d3 to append one table row `tr` for each sighting report object
@@ -45,10 +45,10 @@ data.forEach(function(data) {
 
 // Step 5: Use d3 to update each cell's text with
 // sighting report values 
-data.forEach(function(data) {
-  console.log(data);
+data.forEach(function(tableData) {
+  console.log(tableData);
   var row = tbody.append("tr");
-  Object.entries(data).forEach(function([key, value]) {
+  Object.entries(tableData).forEach(function([key, value]) {
     console.log(key, value);
     // Append a cell to the row for each value
     // in the sighting report object
@@ -56,3 +56,35 @@ data.forEach(function(data) {
     cell.text(value);
   });
 });
+
+//Use a date form  and search through the date/time column to find rows that match user input.
+
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    console.log(inputValue);
+    console.log(tableData);
+  
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+  
+    console.log(filteredData);
+};
